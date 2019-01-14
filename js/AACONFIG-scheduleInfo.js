9bd2f20e112728999b15741ab513dba15322316a
gzhelezo <44788337+gzhelezo@users.noreply.github.com>
2019-01-13 19:40:23 -0500

    Update AACONFIG-scheduleInfo.js

--- js/AACONFIG-scheduleInfo.js
+++ js/AACONFIG-scheduleInfo.js
@@ -4,38 +4,43 @@ courseInfo.classPeriods = [
 	{
 		topic: new Topic("Course Overview", "lectures/lecture01.html"),
 		reading: new Reading("Chapter 1"),
-		lab: new Lab("Labs Overview ", "lab01.html")		
+		lab: new Lab("Lab Module 1 - Overview", "lab01.html")		
 	},{
 		topic: new Topic("Physical Security", "lectures/lecture02.html"),
 		reading: new Reading("Chapter 2"),
-		lab: new Lab("Lab Module 1", "lab02.html")
+		lab: new Lab("Lab Module 2", "lab02.html")
 	},{
 		topic: new Topic("Locks, smart cards, computer forensics", "lectures/lecture02.html"),
 		reading: new Reading("Chapter 2"),
-		lab: new Lab("Lab Module 1", "lab02.html")
+		lab: new Lab("Lab Module 2", "lab02.html")
 	},{
 		topic: new Topic("Operating Systems Security", "lectures/lecture03.html"),
 		reading: new Reading("Chapter 3"),
-		lab: new Lab("Lab Module 2", "lab03.html")
+		lab: new Lab("Lab Module 3", "lab03.html")
 	},{
 		topic: new Topic("Monitoring, filesystem security, buffer-overflow attacks", "lectures/lecture03.html"),
 		reading: new Reading("Chapter 3"),
-		lab: new Lab("Lab Module 2", "lab03.html")
+		lab: new Lab("Lab Module 3", "lab03.html")
 	},{
 		topic: new Topic("Malware. Logic bombs", "lectures/lecture04.html"),
 		reading: new Reading("Chapter 4"),
+		lab: new Lab("Lab Module 4", "lab04.html")
 	},{
 		topic: new Topic("Trojan horses, viruses, worms, rootkits, adware, spyware", "lectures/lecture04.html"),
 		reading: new Reading("Chapter 4"),
+		lab: new Lab("Lab Module 4", "lab04.html")
 	},{
 		topic: new Topic("Network Security I", "lectures/lecture05.html"),
 		reading: new Reading("Chapter 5"),
+		lab: new Lab("Lab Module 5", "lab05.html")
 	},{
 		topic: new Topic("Network Security I", "lectures/lecture05.html"),
 		reading: new Reading("Chapter 5"),
+		lab: new Lab("Lab Module 5", "lab05.html")
 	},{
 		topic: new Topic("Network Security II", "lectures/lecture06.html"),
 		reading: new Reading("Chapter 6"),
+		lab: new Lab(null)
 	},{
 		topic: new Topic("Network Security II", "lectures/lecture06.html"),
 		reading: new Reading("Chapter 6"),
