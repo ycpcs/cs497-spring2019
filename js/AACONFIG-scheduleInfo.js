490683150996646c586684f0de972f1edcc1b22d
gzhelezo <44788337+gzhelezo@users.noreply.github.com>
2019-01-13 19:09:55 -0500

    Update AACONFIG-scheduleInfo.js

--- js/AACONFIG-scheduleInfo.js
+++ js/AACONFIG-scheduleInfo.js
@@ -3,71 +3,72 @@
 courseInfo.classPeriods = [
 	{
 		topic: new Topic("Course Overview", "lectures/lecture01.html"),
-		reading: new Reading("Goodrich: Chapter 1"),
+		reading: new Reading("Chapter 1"),
 		lab: new Lab("Labs Overview ", "lab01.html")		
 	},{
 		topic: new Topic("Physical Security", "lectures/lecture02.html"),
-		reading: new Reading("Goodrich: Chapter 2"),
+		reading: new Reading("Chapter 2"),
 		lab: new Lab("Lab Module 1", "lab02.html")
 	},{
 		topic: new Topic("Locks, smart cards, computer forensics", "lectures/lecture02.html"),
-		reading: new Reading("Goodrich: Chapter 2"),
+		reading: new Reading("Chapter 2"),
 		lab: new Lab("Lab Module 1", "lab02.html")
 	},{
 		topic: new Topic("Operating Systems Security", "lectures/lecture03.html"),
-		reading: new Reading("Goodrich: Chapter 3"),
+		reading: new Reading("Chapter 3"),
 		lab: new Lab("Lab Module 2", "lab03.html")
 	},{
 		topic: new Topic("Monitoring, filesystem security, buffer-overflow attacks", "lectures/lecture03.html"),
-		reading: new Reading("Goodrich: Chapter 3"),
+		reading: new Reading("Chapter 3"),
 		lab: new Lab("Lab Module 2", "lab03.html")
 	},{
 		topic: new Topic("Malware. Logic bombs", "lectures/lecture04.html"),
-		reading: new Reading("Goodrich: Chapter 4"),
+		reading: new Reading("Chapter 4"),
 	},{
 		topic: new Topic("Trojan horses, viruses, worms, rootkits, adware, spyware", "lectures/lecture04.html"),
-		reading: new Reading("Goodrich: Chapter 4"),
+		reading: new Reading("Chapter 4"),
 	},{
 		topic: new Topic("Network Security I", "lectures/lecture05.html"),
-		reading: new Reading("Goodrich: Chapter 5"),
+		reading: new Reading("Chapter 5"),
 	},{
 		topic: new Topic("Network Security I", "lectures/lecture05.html"),
-		reading: new Reading("Goodrich: Chapter 5"),
+		reading: new Reading("Chapter 5"),
 	},{
 		topic: new Topic("Network Security II", "lectures/lecture06.html"),
-		reading: new Reading("Goodrich: Chapter 6"),
+		reading: new Reading("Chapter 6"),
 	},{
 		topic: new Topic("Network Security II", "lectures/lecture06.html"),
-		reading: new Reading("Goodrich: Chapter 6"),
+		reading: new Reading("Chapter 6"),
 	},{
 		topic: new Topic("Web Browser Security", "lectures/lecture07.html"),
-		reading: new Reading("Goodrich: Chapter 7"),
+		reading: new Reading("Chapter 7"),
 	},{
 		topic: new Topic("Web Browser Security", "lectures/lecture07.html"),
-		reading: new Reading("Goodrich: Chapter 7"),
+		reading: new Reading("Chapter 7"),
 	},{
 		topic: new Topic("Cryptography", "lectures/lecture08.html"),
-		reading: new Reading("Goodrich: Chapter 8"),
+		reading: new Reading("Chapter 8"),
 	},{
 		topic: new Topic("Cryptography", "lectures/lecture08.html"),
-		reading: new Reading("Goodrich: Chapter 8"),
+		reading: new Reading("Chapter 8"),
 	},{
 		topic: new Topic("Applications - Email", "lectures/lecture09.html"),
-		reading: new Reading("Goodrich: Chapter 10"),
+		reading: new Reading("Chapter 10"),
 	},{
 		topic: new Topic("Applications - Payment Systems", "lectures/lecture09.html"),
-		reading: new Reading("Goodrich: Chapter 10"),
+		reading: new Reading("Chapter 10"),
 	},{
 		topic: new Topic("Applications - Voting systems", "lectures/lecture10.html"),
-		reading: new Reading("Goodrich: Chapter 10"),
+		reading: new Reading("Chapter 10"),
 	},{
 		topic: new Topic("Applications - Social Networking", "lectures/lecture10.html"),
-		reading: new Reading("Goodrich: Chapter 10"),
+		reading: new Reading("Chapter 10"),
 	},{
 		topic: new Topic("Web Security", "lectures/lecture11.html"),
-		reading: new Reading("Discussion"),
+		reading: new Reading("In class discussion"),
 	},{
-		topic: new Topic("Privacy and Anonymity on the Internet", "lectures/lecture11.html")
+		topic: new Topic("Privacy and Anonymity on the Internet", "lectures/lecture11.html"),
+		reading: new Reading("In class discussion"),
 	},{
 		topic: new Topic("TBD", "lectures/lecture12.html")
 	},{
