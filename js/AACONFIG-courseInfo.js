6189ebc74c33a96d41faac5173461d19942f85b0
gzhelezo <44788337+gzhelezo@users.noreply.github.com>
2019-01-17 21:43:39 -0500

    Update AACONFIG-courseInfo.js

--- js/AACONFIG-courseInfo.js
+++ js/AACONFIG-courseInfo.js
@@ -11,6 +11,12 @@ var courseInfo = {
 	// The following is true if final exam is on the LAST day of class;
 	// false if final exam is during exam week.
 	inClassFinalExam: false,
+	
+	// The following is for the college-scheduled final exam.
+	// It is not used if final is on last day of class
+	courseInfo.finalExamDates = [
+		new FinalExamDay("801", new Date("05/13/2018 19:00:00")),
+	];
 };
 
 // Final exam days and class periods are added in AACONFIG-scheduleInfo.js
